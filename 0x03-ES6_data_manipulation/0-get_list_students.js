6dbb6da0c799cd12cc36b9e27b80be55c30f8489
Nazarene Gena <genarene96@gmail.com>
2023-10-03 15:33:48 +0300

    file added

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,22 +1,7 @@
-// Create a function named getListStudents that returns an array of objects.
-
 export default function getListStudents() {
   return [
-    {
-      firstName: "Guillaume",
-      id: 1,
-      location: "in San Francisco",
-    },
-    {
-      firstName: "James",
-      id: 2,
-      location: "in Columbia",
-    },
-    {
-      firstName: "Serena",
-      id: 5,
-      location: "in San Francisco",
-    },
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" },
   ];
 }
-console.log(getListStudents());
